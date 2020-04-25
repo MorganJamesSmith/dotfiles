2779d4e09bfaff6d151c5931cb1d1415ce67f2e5
Morgan Smith <Morgan.J.Smith@outlook.com>
2020-04-25 13:41:37 -0400

    Changed caching to use gnus-agent, also now I use imap

--- .config/emacs/gnus.el
+++ .config/emacs/gnus.el
@@ -10,6 +10,7 @@
 (require 'nnmaildir)
 (require 'smtpmail)
 (require 'gnus-sum)
+(require 'gnus-agent)
 
 ;; Exit gnus on Emacs exit
 (defun exit-gnus-on-exit ()
@@ -24,31 +25,16 @@
 (customize-set-variable 'send-mail-function         'smtpmail-send-it) ; not for gnus
 (customize-set-variable 'message-send-mail-function 'smtpmail-send-it)
 
-(customize-set-variable 'gnus-cache-enter-articles '(ticked dormant unread read))
-(customize-set-variable 'gnus-cache-remove-articles nil)
-(customize-set-variable 'gnus-cacheable-groups "^nnimap")
-(customize-set-variable 'gnus-use-cache t)
-
-(customize-set-variable 'gnus-asynchronous t)
-(customize-set-variable 'gnus-async-prefetch-article-p (lambda (article) t))
-(customize-set-variable 'gnus-prefetched-article-deletion-strategy nil)
-(customize-set-variable 'gnus-use-article-prefetch t)
-(customize-set-variable 'gnus-use-header-prefetch t)
-
-(customize-set-variable 'mail-source-directory (file-name-as-directory (expand-file-name "Mail" gnus-home-directory)))
-(customize-set-variable 'nnml-directory "~/.config/emacs/gnus-files/News/cache/")
-(unless (file-exists-p mail-source-directory)
-  (make-directory mail-source-directory t))
-
-(customize-set-variable 'mail-source-delete-incoming nil)
-
 ;; I don't want to use a .newsrc file
 (customize-set-variable 'gnus-save-newsrc-file nil)
 (customize-set-variable 'gnus-read-newsrc-file nil)
 
+;; Don't ask how many messages I want to see. I want them all
+(customize-set-variable 'gnus-large-newsgroup nil)
 
-(customize-set-variable 'gnus-inhibit-user-auto-expire t)
-
+;; Sanitize the mail a little
+(add-hook 'nnmail-prepare-incoming-header-hook #'nnmail-remove-leading-whitespace)
+(add-hook 'nnmail-prepare-incoming-header-hook #'nnmail-remove-tabs)
 
 ;; Select first article instead of first unread
 (customize-set-variable 'gnus-auto-select-subject 'first)
@@ -57,12 +43,15 @@
 ;; All groups are new each time gnus is run
 (customize-set-variable 'gnus-save-killed-list nil)
 
+;; If you get a new newsgroup, manually run M-x gnus-find-new-newsgroups
+(customize-set-variable 'gnus-check-new-newsgroups nil)
+
 (customize-set-variable 'gnus-ignored-newsgroups nil)
 
-;; Subscribe to new groups alphabetically
-(customize-set-variable 'gnus-subscribe-newsgroup-method #'gnus-subscribe-alphabetically)
-
 (customize-set-variable 'gnus-message-archive-group nil)
+
+;; Add articles I look at to the cache
+(add-hook 'gnus-select-article-hook #'gnus-agent-fetch-selected-article)
 
 ;; Encrypt email by default and also encrypt to self
 (add-hook 'message-setup-hook 'mml-secure-message-encrypt)
@@ -72,60 +61,77 @@
 ;; Always show all my groups
 (customize-set-variable 'gnus-permanently-visible-groups ".*")
 
+(customize-set-variable 'gnus-parameters '((".*" (display . all))))
 
-(defvar user-mail-address)
-(defvar smtpmail-smtp-user)
-(defvar smtpmail-smtp-server)
-(defvar smtpmail-smtp-service)
-(setq gnus-newsgroup-variables '(user-mail-address smtpmail-smtp-user smtpmail-smtp-server smtpmail-smtp-service))
-(customize-set-variable 'gnus-parameters
-                        `((".*" (display . all))
-                          (".*work.*"
-                           (user-mail-address ,(auth-source-pass-get 'secret "email/work/address"))
-                           (smtpmail-smtp-user ,(auth-source-pass-get 'secret "email/work/address"))
-                           (smtpmail-smtp-server ,(auth-source-pass-get 'secret "email/work/smtp-server"))
-                           (smtpmail-smtp-service  ,(string-to-number (auth-source-pass-get 'secret "email/work/smtp-service"))))
-                          (".*morganjsmith.*"
-                           (user-mail-address "Morgan.J.Smith@outlook.com")
-                           (smtpmail-mail-address "Morgan.J.Smith@outlook.com")
-                           (smtpmail-smtp-server "smtp-mail.outlook.com")
-                           (smtpmail-smtp-service 587))
-                          (".*morganjsmith:emacs.*"
-                           (to-list . "emacs-devel@gnu.org"))
-                          (".*cmail.*"
-                           (user-mail-address "MorganSmith@cmail.carleton.ca")
-                           (smtpmail-mail-address "MorganSmith@cmail.carleton.ca")
-                           (smtpmail-smtp-server "smtp-mail.outlook.com")
-                           (smtpmail-smtp-service 587))
-                          (".*grommin.*"
-                           (user-mail-address "grommin@hotmail.com")
-                           (smtpmail-mail-address "grommin@hotmail.com")
-                           (smtpmail-smtp-server "smtp-mail.outlook.com")
-                           (smtpmail-smtp-service 587))
-                          (".*hotbutterypancake.*"
-                           (user-mail-address "hotbutterypancake@gmail.com")
-                           (smtpmail-mail-address "hotbutterypancake@gmail.com")
-                           (smtpmail-smtp-server "smtp.gmail.com")
-                           (smtpmail-smtp-service 587))))
-
+;; Receiving email stuff
 
 (customize-set-variable 'gnus-select-method '(nnnil ""))
 (customize-set-variable 'gnus-secondary-select-methods
-                        `((nnmaildir "morganjsmith"
-                                     (directory "~/.local/share/mail/morganjsmith/"))
-                          (nnmaildir "cmail"
-                                     (directory "~/.local/share/mail/cmail/"))
-                          (nnmaildir "grommin"
-                                     (directory "~/.local/share/mail/grommin/"))
-                          (nnmaildir "hotbutterpancake"
-                                     (directory "~/.local/share/mail/hotbutterypancake/"))
+                        `((nnimap "morganjsmith"
+                                  (nnimap-user "morgan.j.smith@outlook.com")
+                                  (nnimap-address "imap-mail.outlook.com")
+                                  (nnimap-authenticator login))
+                          (nnimap "cmail"
+                                  (nnimap-user "morgansmith@cmail.carleton.ca")
+                                  (nnimap-address "imap-mail.outlook.com")
+                                  (nnimap-authenticator login))
+                          (nnimap "grommin"
+                                  (nnimap-user "grommin@hotmail.com")
+                                  (nnimap-address "imap-mail.outlook.com")
+                                  (nnimap-authenticator login))
+                          (nnimap "hotbutterpancake"
+                                  (nnimap-user "hotbutterypancake@gmail.com")
+                                  (nnimap-address "imap.gmail.com")
+                                  (nnimap-authenticator login))
                           (nnimap "work"
                                   (nnimap-user ,(auth-source-pass-get 'secret "email/work/address"))
                                   (nnimap-address ,(auth-source-pass-get 'secret "email/work/imap-server"))
                                   (nnimap-server-port ,(string-to-number (auth-source-pass-get 'secret "email/work/imap-port")))
                                   (nnimap-authenticator xoauth2))))
 
+;; Sending email stuff
 
+(defun compose-mail-choose-account ()
+  "Call `compose-mail' after setting up the environment for a specific account."
+  (interactive)
+
+  (let ((account (ido-completing-read
+                  "Choose account: "
+                  '("morganjsmith" "cmail" "grommin" "hotbutterypancake" "work"))))
+    (cond ((equal account "morganjsmith")
+           (setq
+            user-mail-address "Morgan.J.Smith@outlook.com"
+            smtpmail-smtp-server "smtp-mail.outlook.com"
+            smtpmail-smtp-service 587
+            smtpmail-auth-supported '(login)))
+          ((equal account "cmail")
+           (setq
+            user-mail-address "morgansmith@cmail.carleton.ca"
+            smtpmail-smtp-server "smtp-mail.outlook.com"
+            smtpmail-smtp-service 587
+            smtpmail-auth-supported '(login)))
+          ((equal account "grommin")
+           (setq
+            user-mail-address "grommin@hotmail.com"
+            smtpmail-smtp-server "smtp-mail.outlook.com"
+            smtpmail-smtp-service 587
+            smtpmail-auth-supported '(login)))
+          ((equal account "hotbutterypancake")
+           (setq
+            user-mail-address "hotbutterypancake@gmail.com"
+            smtpmail-smtp-server "smtp.gmail.com"
+            smtpmail-smtp-service 587
+            smtpmail-auth-supported '(login)))
+          ((equal account "work")
+           (setq
+            user-mail-address (auth-source-pass-get 'secret "email/work/address")
+            smtpmail-smtp-server (auth-source-pass-get 'secret "email/work/smtp-server")
+            smtpmail-smtp-service  (string-to-number (auth-source-pass-get 'secret "email/work/smtp-service")))
+            smtpmail-auth-supported '(xoauth2)))
+
+    (setq smtpmail-smtp-user account)
+    (compose-mail)))
+(bind-key "C-x m" #'compose-mail-choose-account)
 
 ;; Make stuff pretty section
 
