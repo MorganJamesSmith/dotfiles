5b85d9246116caae0bc35535f3b3e28f631a8a32
Morgan Smith <Morgan.J.Smith@outlook.com>
2020-08-26 21:37:15 -0400

    remove problematic keybindings

--- .config/nyxt/init.lisp
+++ .config/nyxt/init.lisp
@@ -1,7 +1,2 @@
 (define-configuration buffer
-  ((default-modes (append '(vi-normal-mode) %slot-default))
-   (override-map (let ((map (make-keymap "override-map")))
-                   (define-key map
-                     "g d" 'delete-current-buffer
-                     ">" 'switch-buffer-next
-                     "<" 'switch-buffer-previous)))))
+  ((default-modes (append '(vi-normal-mode) %slot-default))))
