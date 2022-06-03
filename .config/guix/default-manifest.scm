43e2d7ebaf5d67372f0f7279a09fe9be68e7c2af
Morgan Smith <Morgan.J.Smith@outlook.com>
2022-06-03 18:19:51 -0400

    Update default-manifest.scm

--- .config/guix/default-manifest.scm
+++ .config/guix/default-manifest.scm
@@ -11,20 +11,20 @@
     "yt-dlp")) ; Downloads more than just YouTube
 
 (define programming
-  '("git"
-    "git:send-email"
-    "guile"))
+  '("plantuml"
+    "global"
+    "git"
+    "git:send-email"))
 
-(define web-browsing
-  '("firefox"
-    "qutebrowser"))
+(define email
+  '("msmtp" ; sending emails
+    "isync" ; receiving emails
+    "rss2email"))
 
-(define desktop-environment
-  '("sway"
-    "swayidle"
-    "bemenu"
-    "qtwayland"
-    "qt5ct"))
+(define terminal-tools
+  '("file" ; filetype checker
+    "tree"
+    "htop"))
 
 (define stuff-only-needed-for-their-environment-variables
   '("man-db"     ;; MANPATH
@@ -35,43 +35,24 @@
   audio
   downloaders
   programming
-  web-browsing
-  desktop-environment
+  email
+  terminal-tools
   stuff-only-needed-for-their-environment-variables
   '("aspell" ; spellchecker
     "aspell-dict-en"
     "brightnessctl"
-    "ffmpeg"
-    "file" ; filetype checker
     "flameshot" ; Screenshots
     "flatpak"
-    "global"
     "gnupg"
-    "htop"
-    "icedove" ; email client
     "libreoffice"
-    "lxqt-policykit" ; used in xinitrc
-    "make"
     "man-pages"
     "mpv" ; video player
     "openscad" ; 3D modeling program
     "openssh"
     "password-store"
-    "picom" ; compositor
-    "pinentry"
-    "pulseaudio"
     "pwgen" ; Password Generator
     "rmlint"
-    "rsync"
-    "sx" ; Start X
-    "sxiv"
-    "tree"
-    "unclutter"
+    "recutils"
     "unzip"
     "xpdf" ; pdftotext
-    "xrandr"
-    "xhost"    ; used in xinitrc
-    "xset"     ; used in xinitrc
-    "xsetroot" ; used in xinitrc
-    "xss-lock" ; auto lock screen (run in xinitrc)
     "zip")))
