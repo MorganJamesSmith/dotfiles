2d7a1874b760df801e14cfd1f8148d1db4213ac2
Morgan Smith <Morgan.J.Smith@outlook.com>
2022-06-03 18:30:49 -0400

    Update emacs-manifest.scm

--- .config/guix/emacs-manifest.scm
+++ .config/guix/emacs-manifest.scm
@@ -7,46 +7,34 @@
 (use-modules
  (flat packages emacs))
 
-(define (emacs-git-commit)
-  (let* ((pipe (with-directory-excursion "/home/pancake/src/emacs/emacs"
+(define (git-commit path)
+  (let* ((pipe (with-directory-excursion path
                  (open-pipe* OPEN_READ "git" "rev-parse" "HEAD")))
          (version (read-line pipe)))
     (close-pipe pipe)
     version))
 
+(define emacs-git-commit (git-commit "/home/pancake/src/emacs/emacs"))
+(define org-git-commit (git-commit "/home/pancake/src/emacs/org-mode"))
+
+
 (define transformations
   (options->transformation
-   `((with-commit  . ,(string-append "emacs-native-comp=" (emacs-git-commit)))
+   `(
+     (with-commit  . ,(string-append "emacs-pgtk-native-comp=" emacs-git-commit))
+     (with-git-url . "emacs-pgtk-native-comp=/home/pancake/src/emacs/emacs")
 
-     (with-git-url . "emacs-native-comp=/home/pancake/src/emacs/emacs")
-     (with-input   . "emacs=emacs-native-comp")
-     (with-input   . "emacs-minimal=emacs-native-comp")
-     (with-input   . "emacs-no-x=emacs-native-comp")
-     (with-input   . "emacs-no-x-toolkit=emacs-native-comp")
+     (with-commit  . ,(string-append "emacs-org=" org-git-commit))
+     (with-git-url . "emacs-org=/home/pancake/src/emacs/org-mode")
 
-     ;; (with-git-url . "emacs-dash=/home/pancake/src/emacs/dash.el")
-     ;; (with-branch  . "emacs-dash=master")
+     (with-input   . "emacs=emacs-pgtk-native-comp")
+     (with-input   . "emacs-minimal=emacs-pgtk-native-comp")
+     (with-input   . "emacs-no-x=emacs-pgtk-native-comp")
+     (with-input   . "emacs-no-x-toolkit=emacs-pgtk-native-comp")
 
+     (without-tests . "emacs-kv")
+     (without-tests . "emacs-org")
      (without-tests . "emacs-yasnippet") ;; Problem with 0.14.0.  Fixed in elpa
-     (with-branch  . "emacs-ledger-mode=master") ;; Problem with 4.0.0
-
-     ;; version: 20200515-1.0ef8b13
-     ;; (wrong-type-argument utf-8-string-p "\177ELF...
-     (without-tests  . "emacs-libgit")
-
-     ;; version: 1.24
-     ;; "Cannot find suitable directory for output in ‘native-comp-eln-load-path’"
-     ;; (without-tests  . "emacs-buttercup")
-
-     ;; version: 2.4.1
-     ;; "Cannot find suitable directory for output in ‘native-comp-eln-load-path’"
-     ;; (without-tests  . "emacs-use-package")
-
-     ;; current value of native-comp-eln-load-path:
-     ;; ("/homeless-shelter/.emacs.d/eln-cache/" "/gnu/store/vix7md2m5lm5j8k8hi7hw8fcjaq518hx-emacs-native-comp-git.9a5a35c/lib/emacs/28.0.50/native-lisp/")
-
-     ;; (without-tests  . "emacs-esup")
-     ;; (without-tests  . "emacs-lispy")
      )))
 
 (define (specifications->manifest-with-transformations packages)
@@ -60,46 +48,43 @@
 
 (define emacs-packages
   (append!
-   '("emacs-native-comp"
-     "graphicsmagick" ; image-dired
-     "libjpeg"        ; image-dired
+   '("emacs-pgtk-native-comp"
      "pinentry-emacs"
      "ghostscript" ; allows Emacs to preview PostScript
      "unoconv")    ; allows Emacs to preview docx files
    (map
     (lambda (x) (string-append "emacs-" x))
-    '("debbugs"
+    '("crdt"
+      "debbugs"
       "delight"
       "desktop-environment"
       "diff-hl"
       "disk-usage"
       "elpher"
       "eshell-syntax-highlighting"
-      "exwm"
       "flymake-shellcheck"
       "geiser"
+      "geiser-guile"
       "ggtags"
       "guix"
-      "highlight-numbers"
-      "irfc"
       "ledger-mode"
       "literate-calc-mode"
+      "logos"
       "magit"
       "nov-el"
-      "org"
-      "org-contacts"
       "org-contrib" ;; for org-passwords
+      "osm"
       "pdf-tools"
       "pinentry"
       "plantuml-mode"
       "rainbow-delimiters"
+      "rec-mode"
       "scad-mode"
       "sr-speedbar"
       "transmission"
-      "typit"
-      "undo-tree"
-      "use-package"
+      "tup-mode"
       "vterm"
+      "vundo"
       "which-key"
       "ws-butler"
       "yasnippet"))))
