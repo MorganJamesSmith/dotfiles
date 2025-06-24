bffde73aff663b0f2dcb810bf2b2d46e23a54a89
Morgan Smith <Morgan.J.Smith@outlook.com>
2025-06-24 13:16:09 -0400

    emacs-manifest.scm: Convert to guile module

--- .config/guix/emacs-manifest.scm
+++ .config/guix/emacs-manifest.scm
@@ -1,4 +1,8 @@
+(define-module (emacs-manifest))
+
 (use-modules
+ (gnu packages)
+ (guix profiles)
  ((ice-9 popen) #:select (open-pipe* close-pipe))
  ((ice-9 rdelim) #:select (read-line))
  (guix cpu)
@@ -57,65 +61,67 @@
 (define (transformations package)
   (patch-transformations (other-transformations package)))
 
-(define (specifications->manifest-with-transformations packages)
-  (packages->manifest
-   (map
-    (compose
-     (lambda (package output)
-       (list (transformations package) output))
-     specification->package+output)
-    packages)))
+(define (specifications->packages-with-transformations packages)
+  (map
+   (compose
+    (lambda (package output)
+      (list (transformations package) output))
+    specification->package+output)
+   packages))
 
-(define emacs-packages
-  (append!
-   '("emacs-next-pgtk"
-     "gnuplot"
-     "shellcheck"  ; flymake shell files
-     "graphicsmagick" ; image-dired thumbnail generation
-     "libjpeg"        ; image-dired rotate images
-     "mupdf"       ; allows Emacs to preview EPUB
-     "ghostscript" ; allows Emacs to preview PostScript
-     "djvulibre"   ; allows Emacs to preview djvu files
-     "libreoffice" ; allows Emacs to preview docx files
-
-     "proof-general" ; elisp package not pre-fixed with "emacs-"
-     "coq")
-   (map
-    (lambda (x) (string-append "emacs-" x))
-    '(
-      "arei"
-      "bluetooth"
-      "crdt"
-      "csv-mode"
-      "debbugs"
-      "delight"
-      "diff-hl"
-      "disk-usage"
-      "elpher"
-      "emms"
-      "eshell-syntax-highlighting"
-      "geiser"
-      "geiser-guile"
-      "ggtags"
+(define-public emacs-manifest-packages
+  (specifications->packages-with-transformations
+   (append!
+    '("emacs-next-pgtk"
       "gnuplot"
-      "guix"
-      "flycheck-ledger"
-      "ledger-mode"
-      "literate-calc-mode"
-      "nov-el"
-      "org"
-      "osm"
-      "pinentry"
-      "rainbow-delimiters"
-      "scad-mode"
-      "smartparens"
-      "transmission"
-      "vterm"
-      "ws-butler"
-      "yasnippet"))))
+      "shellcheck"  ; flymake shell files
+      "graphicsmagick" ; image-dired thumbnail generation
+      "libjpeg"        ; image-dired rotate images
+      "mupdf"       ; allows Emacs to preview EPUB
+      "ghostscript" ; allows Emacs to preview PostScript
+      "djvulibre"   ; allows Emacs to preview djvu files
+      "libreoffice" ; allows Emacs to preview docx files
 
-(specifications->manifest-with-transformations
- emacs-packages)
+      "proof-general" ; elisp package not pre-fixed with "emacs-"
+      "coq")
+    (map
+     (lambda (x) (string-append "emacs-" x))
+     '(
+       "arei"
+       "bluetooth"
+       "crdt"
+       "csv-mode"
+       "debbugs"
+       "delight"
+       "diff-hl"
+       "disk-usage"
+       "elpher"
+       "emms"
+       "eshell-syntax-highlighting"
+       "geiser"
+       "geiser-guile"
+       "ggtags"
+       "gnuplot"
+       "guix"
+       "flycheck-ledger"
+       "ledger-mode"
+       "literate-calc-mode"
+       "nov-el"
+       "org"
+       "osm"
+       "pinentry"
+       "rainbow-delimiters"
+       "scad-mode"
+       "smartparens"
+       "transmission"
+       "vterm"
+       "ws-butler"
+       "yasnippet")))))
+
+(define-public emacs-manifest
+  (packages->manifest emacs-manifest-packages))
+
+emacs-manifest
 
 ;; Local Variables:
 ;; compile-command: "guix build --max-jobs=4 --keep-going -m /home/pancake/.config/guix/emacs-manifest.scm"
