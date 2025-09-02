9d10463522683a8e0491cf41093990461fb06ede
Morgan Smith <Morgan.J.Smith@outlook.com>
2025-09-02 14:50:54 -0400

    Use upstream cyrus-sasl-xoauth2

--- .config/guix/default-manifest.scm
+++ .config/guix/default-manifest.scm
@@ -7,52 +7,6 @@
   (add-to-load-path (dirname (current-filename))))
 (use-modules (transformations))
 
-;; Waiting for this to be accepted upstream: bug#58074
-(define-public cyrus-sasl-xoauth2
-  (eval
-   '(begin
-      (use-modules (guix packages)
-                   (guix git-download)
-                   (guix build-system gnu)
-                   ((guix licenses) #:prefix license:)
-                   (gnu packages cyrus-sasl)
-                   (gnu packages autotools)
-                   (guix gexp))
-      (package
-        (name "cyrus-sasl-xoauth2")
-        (version "0.2")
-        (source (origin
-                  (method git-fetch)
-                  (uri (git-reference
-                        (url "https://github.com/moriyoshi/cyrus-sasl-xoauth2")
-                        (commit (string-append "v" version))))
-                  (file-name (git-file-name name version))
-                  (sha256
-                   (base32
-                    "1py9f1mn5k5xihrk0lfrwr6723c22gjb7lmgya83ibvislm2x3wl"))))
-        (build-system gnu-build-system)
-        (arguments
-         (list #:configure-flags #~(list (string-append "--with-cyrus-sasl="
-                                                        #$output)
-                                         "--disable-static")
-               #:phases #~(modify-phases %standard-phases
-                            (add-before 'bootstrap 'fix-autogen
-                              (lambda _
-                                ;; autogen.sh is executable but does not have a shebang.
-                                (chmod "autogen.sh" #o400))))))
-        (inputs (list cyrus-sasl))
-        (native-inputs (list autoconf automake libtool))
-        (native-search-paths
-         (list (search-path-specification
-                (variable "SASL_PATH")
-                (files (list "lib/sasl2")))))
-        (home-page "https://github.com/moriyoshi/cyrus-sasl-xoauth2")
-        (synopsis "XOAUTH2 plugin for Cyrus SASL")
-        (description "Adds support for XOAUTH2 authentication to Cyrus SASL.  This
-package can be used with isync to fetch mail from servers that support it.")
-        (license license:expat)))
-   (make-fresh-user-module)))
-
 (define audio
   '("alsa-utils" ; alsamixer
     "pulseaudio"
@@ -75,8 +29,7 @@ package can be used with isync to fetch mail from servers that support it.")
 (define email
   '("msmtp" ; sending emails
     "cyrus-sasl"
-    ;; Added below
-    ;; "cyrus-sasl-xoauth2"
+    "cyrus-sasl-xoauth2"
     "isync" ; receiving emails
     "icedove-minimal"
     "rss2email"))
@@ -116,8 +69,7 @@ package can be used with isync to fetch mail from servers that support it.")
       "rmlint"
       "rsync"
       "xpdf" ; pdftotext
-      ))
-   (list cyrus-sasl-xoauth2)))
+      ))))
 
 (define-public default-manifest
   (packages->manifest default-manifest-packages))
